549700a9761d61134ded60f784a1743d8a02728b
Dineshreddysnw <134279967+Dineshreddysnw@users.noreply.github.com>
2023-05-22 16:29:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-stremlit.text('Omega3 and Blueberry Oatmeal')
+streamlit.text('Omega3 and Blueberry Oatmeal')
